108bf55d6a515f47eed683a2d7067530f0f6424d
Bastien-Project <b.thomas30712@gmail.com>
2025-07-21 13:31:14 +0200

    DEL : slider et font Poppins

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
 <?php include 'header.php'; ?>
 
 <body>
-    <h1>Retrouvez nos offres black friday</h1>
+    <h1>Alibabouche - Le souk en ligne</h1>
     <div id="blackF">
-        <img src="images/black friday.jpg">
+        <img src="images/alibabouche.png" alt="Logo Alibabouche" width="auto" height="500vh">
     </div>
-    <h3>Meilleurs ventes</h3>
-    <div class="slider basIndex" id="main-slider">
-        <div class="slider-wrapper">
-            <img src="images/nike/Dunk/Nike Dunk Low/1.png" alt="First" class="slide" width="100px" height="100px" /><!-- slides -->
-            <img src="images/nike/Air force 1/Air Force 1'07 full white/1.png" alt="Second" class="slide" width="100px" height="100px" />
-            <img src="images/nike/Blazer/blazer MID'77Sigle Jaune/1.png" alt="Third" class="slide" width="100px" height="100px" />
-        </div>
-    </div>
-    <img id="slider" name="slide">
 </body>
 
 <?php include 'footer.php'; ?>
