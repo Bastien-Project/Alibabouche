877da2da5fcbc82a218b3f645446232db7ef6aaf
Bastien-Project <b.thomas30712@gmail.com>
2025-07-21 10:52:53 +0200

    ADD : création fichier hearder et footer FIX : change page HTML en PHP FIX : inclut header et footer dans les pages PHP

---
+++ footer.php
@@ -0,0 +1,29 @@
+<footer class="bottomBas">
+        <div id="footer_marques">
+            <p><strong>Marques:</strong></p>
+            <ul>
+                <li><a href="nike.html" target="_blank">Nike</a></li>
+                <li><a href="adidas.html" target="_blank">Adidas</a></li>
+                <li><a href="jordans.html" target="_blank">Jordans</a></li>
+                <li><a href="babouche.html" target="_blank">Babouche</a></li>
+            </ul>
+        </div>
+        <div id="footer_condition">
+            <p><strong>Informations légales:</strong></p>
+            <ul>
+                <li>Conditions générales et politique de conficdentialite</li>
+                <li>Chaine d'approvisionnement</li>
+                <li>Politique sur les Cookies et la Confidentialite</li>
+                <li>Paramètres des Cookies</li>
+
+            </ul>
+        </div>
+        <div id="footer_left">
+            <div id="reseaux">
+                <a id="icon" href="https://www.instagram.com/alibabouche_officiel/" target="_blank"><i class="uil uil-instagram"></i></a>
+                <a id="icon" href="https://twitter.com/AliBABOUCHE_Off" target="_blank"><i class="uil uil-twitter-alt"></i></a>
+            </div>
+            <p id="inc">© 2021 Alibabouche, Inc. Tous droits réservés</p>
+        </div>
+    </footer>
+</html>
---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="slider.css">
+		<script type="text/javascript" src="slider.js"></script>
+        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+        <title>Alibabouche</title>
+    </head>
+    <header>
+        <a href="#" class="logo">Alibabouche</a>
+        <ul>
+            <li><a href="index.html" class="active">Home</a></li>
+            <li><a href="nike.html">Nike</a></li>
+            <li><a href="adidas.html">Adidas</a></li>
+            <li><a href="aboutUs.html">About Us</a></li>
+            <li><img src="images/panier.png" width="30px" height="30px"></li>
+        </ul>
+    </header>
